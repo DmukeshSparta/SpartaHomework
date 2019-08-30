1ef594279a67f06cd7933011df56adcd5f9ec490
Dipak Mukesh <DMukesh@spartaglobal.com>
2019-08-30 11:56:19 +0100

    Update Lab1OutProperties code

--- PropertiesHomework/PropertiesHomeworks/Lab1OutProperties/Program.cs
+++ PropertiesHomework/PropertiesHomeworks/Lab1OutProperties/Program.cs
@@ -6,21 +6,24 @@ namespace Lab1OutProperties
     {
         static void Main(string[] args)
         {
-            /*
-             * Declare a method which inputs three integers and returns the product 
-             * ie the value of the three numbers multiplied together. Also return an out 
-             * parameter which contains the sum of the three integers.
+            /*Declare a method which inputs three integers and returns the 
+             * product ie the value of the three numbers multiplied together. 
+             * Also return an out parameter which contains the sum of the three integers.
              */
 
-            int addition;
             int sum = 0;
 
-            Console.WriteLine("The Addition of the Value: {0}", sum);
-            Console.WriteLine("The Multiplication of the value: {0}", sum);
+            Program p = new Program();
+
+            int addnumber = p.Integers(20, 30, 40, out sum);
+            int multiplenumber = p.Integers(20, 30, 40, out addnumber);
+
+            Console.WriteLine("The Addition of the Value: {0}", addnumber);
+            Console.WriteLine("The Multiplication of the value: {0}", multiplenumber);
             Console.ReadLine();
 
         }
-        public int Addition(int x, int y, int z, out int sum)
+        public int Integers(int x, int y, int z, out int sum)
         {
             sum = x + y + z;
             return x * y * z;
